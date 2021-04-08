2a7539df696d608138f8536cb4ff14929df6a8f8
Yegor Gangalo <gangaloyegor@gmail.com>
2021-04-08 12:10:54 +0300

    try4

--- routes/controllers/commentsAPI.js
+++ routes/controllers/commentsAPI.js
@@ -1,48 +1,58 @@
-const { CommentModel } = require('../models/models')
+const { CommentModel } = require('../models/models');
 
 const handlerReturn = (result, successCode = 200) => ({
-    code: result ? successCode : 404,
-    data: result ? result : { status: "error", message: "Not found" }
-})
-
-const handlerMongoGetAll = async (Model) => {
-    const result = await Model.find({})
-    return handlerReturn(result)
-}
+  code: result ? successCode : 404,
+  data: result ? result : { status: 'error', message: 'Not found' },
+});
 
 const handlerMongoGetById = async (Model, req) => {
-    const result = await Model.findOne({ _id: req.params.id })
-    return handlerReturn(result)
-}
+  const result = await Model.findOne({ _id: req.params.id });
+  return handlerReturn(result);
+};
+
+const handlerMongoGetAll = async Model => {
+  const result = await Model.find({});
+  return handlerReturn(result);
+};
 
 const handlerMongoPost = async (Model, req) => {
-    const result = await Model.create(req.body);
-    return handlerReturn(result, 201)
-}
+  const result = await Model.create(req.body);
+  return handlerReturn(result, 201);
+};
 
 const handlerMongoDelete = async (Model, req) => {
-    const result = await Model.findOneAndRemove({ _id: req.params.id })
-    return handlerReturn(result)
-}
+  const result = await Model.findOneAndRemove({ _id: req.params.id });
+  return handlerReturn(result);
+};
 
 const handlerMongoPatch = async (Model, req) => {
-    const result = await Model.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
-    return handlerReturn(result)
-}
+  const result = await Model.findOneAndUpdate(
+    { _id: req.params.id },
+    req.body,
+    { new: true },
+  );
+  return handlerReturn(result);
+};
 
 const controller = (handlerMongo, Model) => async (req, res, next) => {
-    try {
-        const { code, data } = await handlerMongo(Model, req)
-        return res.status(code).send(data);
-    } catch (err) {
-        next(err)
-    }
-}
+  try {
+    const { code, data } = await handlerMongo(Model, req);
+    return res.status(code).send(data);
+  } catch (err) {
+    next(err);
+  }
+};
 
-const getDataMongo = controller(handlerMongoGetAll, CommentModel)
-const getByIdDataMongo = controller(handlerMongoGetById, CommentModel)
-const postDataMongo = controller(handlerMongoPost, CommentModel)
-const deleteDataMongo = controller(handlerMongoDelete, CommentModel)
-const patchDataMongo = controller(handlerMongoPatch, CommentModel)
+const getDataMongo = controller(handlerMongoGetAll, CommentModel);
+const getByIdDataMongo = controller(handlerMongoGetById, CommentModel);
+const postDataMongo = controller(handlerMongoPost, CommentModel);
+const deleteDataMongo = controller(handlerMongoDelete, CommentModel);
+const patchDataMongo = controller(handlerMongoPatch, CommentModel);
 
-module.exports = {getDataMongo, getByIdDataMongo, postDataMongo, deleteDataMongo, patchDataMongo}
+module.exports = {
+  getDataMongo,
+  getByIdDataMongo,
+  postDataMongo,
+  deleteDataMongo,
+  patchDataMongo,
+};
