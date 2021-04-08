e56c606a50542ad09a8244c8c123e0e5f1105274
Yegor Gangalo <gangaloyegor@gmail.com>
2021-04-08 12:15:19 +0300

    try6

--- routes/commentsRoutes.js
+++ routes/commentsRoutes.js
@@ -1,15 +1,22 @@
-const express = require('express')
-const router = express.Router()
-const {validateCreateComment, validateUpdateComment} = require('./validation/validationComments')
-const {getDataMongo, getByIdDataMongo, postDataMongo, deleteDataMongo, patchDataMongo} = require('./controllers/commentsAPI')
+const express = require('express');
+const router = express.Router();
+const {
+  validateCreateComment,
+  validateUpdateComment,
+} = require('./validation/validationComments');
+const {
+  getDataMongo,
+  getByIdDataMongo,
+  postDataMongo,
+  deleteDataMongo,
+  patchDataMongo,
+} = require('./controllers/commentsAPI');
 
 router
-    .get('/', getDataMongo)
-    .post('/', validateCreateComment, postDataMongo)
+  .get('/:id', getByIdDataMongo)
+  .delete('/:id', deleteDataMongo)
+  .patch('/:id', validateUpdateComment, patchDataMongo);
 
-router
-    .get('/:id', getByIdDataMongo)
-    .delete('/:id', deleteDataMongo)
-    .patch('/:id', validateUpdateComment, patchDataMongo)
+router.get('/', getDataMongo).post('/', validateCreateComment, postDataMongo);
 
-module.exports = router
+module.exports = router;
